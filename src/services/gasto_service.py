9a161affd1bfd289a4d2f3a6cc074b2c52c237e7
MarcelleDev <devmarcelle94@gmail.com>
2026-03-10 09:07:15 -0300

    Refatora método calcular_total_mes para utilizar dicionários e formatação de data

--- src/services/gasto_service.py
+++ src/services/gasto_service.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+
 class GastoService:
-    def calcular_total_mes(self, lista_de_gastos, mes_desejado, ano_desejado):
-        total = 0
-        for gasto in lista_de_gastos:
-            # Verificamos se o mês e o ano coincidem
-            if gasto.data.month == mes_desejado and gasto.data.year == ano_desejado:
-                total += gasto.valor
+    def calcular_total_mes(self, gastos, mes_desejado, ano_desejado):
+        total = 0.0
+        for gasto in gastos:        
+            data_objeto = datetime.strptime(gasto['data'], "%Y-%m-%d")
+            
+            if data_objeto.month == mes_desejado and data_objeto.year == ano_desejado:
+                total += gasto['valor']
+        
         return total
